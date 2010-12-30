50a403855e15ef290ab977a52072da6ed83c25b0
Chaitanya Gupta <chaitanya.g@directi.com>
2010-12-30 14:52:02 +0530

    Added basic logging.

--- flaky-proxy.lisp
+++ flaky-proxy.lisp
@@ -41,6 +41,7 @@
          (content-length (cdr (assoc :content-length input-headers)))
          (post-data (when (and content-length (plusp (parse-integer content-length)))
                       (hunchentoot:raw-post-data :force-binary t))))
+    (log-message "REQ recd: ~A ~A" request-method uri)
     (call-hook *request-received-hook*)
     (setf input-headers (filter-headers *remove-output-headers* input-headers))
     (multiple-value-bind (body return-code output-headers)
@@ -50,6 +51,7 @@
                              :content post-data
                              :redirect nil
                              :force-binary t)
+      (log-message "RES recd: ~A" return-code)
       (call-hook *response-received-hook* body return-code output-headers)
       (setf output-headers (filter-headers *remove-output-headers* output-headers))
       (loop for (header . value) in output-headers
@@ -85,3 +87,28 @@
   (with-count (*response-received-hook* count)
       (&rest args)
     (hunchentoot:abort-request-handler)))
+
+;;; Logging
+
+(defvar *log-file* #p"/tmp/flaky-proxy.log")
+(defvar *log-file-stream* nil)
+
+(defvar *id-lock* (bordeaux-threads:make-lock "id"))
+(defvar *id* 1)
+
+(defun iso-time (&optional (time (get-universal-time)))
+  "Returns the universal time TIME as a string in full ISO format."
+  (multiple-value-bind (second minute hour date month year)
+      (decode-universal-time time)
+    (format nil "~4,'0d-~2,'0d-~2,'0d ~2,'0d:~2,'0d:~2,'0d"
+            year month date hour minute second)))
+
+(defun log-message (fmt &rest args)
+  (format *log-file-stream* "~&[~A] #~A ~A~%"
+          (iso-time)
+          *id*
+          (apply #'format nil fmt args))
+  (force-output *log-file-stream*))
+
+(defun init ()
+  (setf *log-file-stream* (open *log-file* :direction :output :if-exists :append :if-does-not-exist :create)))
