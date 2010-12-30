2bd886cf95c880ae41ee6df8ea8283535531a489
Chaitanya Gupta <chaitanya.g@directi.com>
2010-12-30 14:26:43 +0530

    Added eat-request/response hooks.

--- flaky-proxy.lisp
+++ flaky-proxy.lisp
@@ -26,6 +26,14 @@
              headers
              :key #'car))
 
+;;; Hooks
+(defvar *request-received-hook* nil)
+(defvar *response-received-hook* nil)
+
+(defun call-hook (hook &rest args)
+  (when hook
+    (apply hook args)))
+
 (defun proxy-handler ()
   (let* ((request-method (hunchentoot:request-method*))
          (uri (hunchentoot:request-uri*))
@@ -33,6 +41,7 @@
          (content-length (cdr (assoc :content-length input-headers)))
          (post-data (when (and content-length (plusp (parse-integer content-length)))
                       (hunchentoot:raw-post-data :force-binary t))))
+    (call-hook *request-received-hook*)
     (setf input-headers (filter-headers *remove-output-headers* input-headers))
     (multiple-value-bind (body return-code output-headers)
         (drakma:http-request uri
@@ -41,6 +50,7 @@
                              :content post-data
                              :redirect nil
                              :force-binary t)
+      (call-hook *response-received-hook* body return-code output-headers)
       (setf output-headers (filter-headers *remove-output-headers* output-headers))
       (loop for (header . value) in output-headers
          do (setf (hunchentoot:header-out header) value))
@@ -48,3 +58,30 @@
         (setf (hunchentoot:content-length*) (length body)))
       (setf (hunchentoot:return-code*) return-code)
       body)))
+
+;;; Hook handlers
+
+(defun clear-hooks ()
+  (setf *request-received-hook* nil
+        *response-received-hook* nil))
+
+(defmacro with-count ((place count) (&rest args) &body body)
+  (let ((count-var (gensym)))
+    `(let ((,count-var ,count))
+       (setf ,place (lambda (,@args)
+                      (when (or (null ,count-var) (plusp ,count-var))
+                        (when (integerp ,count-var)
+                          (decf ,count-var)
+                          (when (zerop ,count-var)
+                            (setf ,place nil)))
+                        ,@body))))))
+
+(defun eat-request (&key count)
+  (with-count (*request-received-hook* count)
+      ()
+    (hunchentoot:abort-request-handler)))
+
+(defun eat-response (&key count)
+  (with-count (*response-received-hook* count)
+      (&rest args)
+    (hunchentoot:abort-request-handler)))
